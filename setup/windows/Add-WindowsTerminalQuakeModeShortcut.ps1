7f553d15dffcfa0ba50fa3d88760d9f344467e49
Norman Dankert <norman.dankert@outlook.com>
2022-11-18 22:02:55 +0100

    add script for quake mode terminal shortcut

---
+++ setup/windows/Add-WindowsTerminalQuakeModeShortcut.ps1
@@ -0,0 +1,9 @@
+$SourceFilePath = (Get-Command wt).Source
+$StartupFolder = [Environment]::GetFolderPath([Environment+SpecialFolder]::Startup)
+$ShortcutPath = Join-Path -Path $StartupFolder "Windows Terminal Quake Mode.lnk"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
+$shortcut.TargetPath = $SourceFilePath
+$shortcut.WorkingDirectory = Split-Path -Parent $SourceFilePath
+$shortcut.Arguments = "-w _quake"
+$shortcut.Save()
