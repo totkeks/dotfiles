bfdef098263047793377ca05cafe90298371ffb9
Norman Dankert <norman.dankert@outlook.com>
2022-11-19 21:34:36 +0100

    style changes

--- setup/windows/Add-WindowsTerminalQuakeModeShortcut.ps1
+++ setup/windows/Add-WindowsTerminalQuakeModeShortcut.ps1
@@ -1,7 +1,8 @@
 $SourceFilePath = (Get-Command wt).Source
 $StartupFolder = [Environment]::GetFolderPath([Environment+SpecialFolder]::Startup)
 $ShortcutPath = Join-Path -Path $StartupFolder "Windows Terminal Quake Mode.lnk"
-$WScriptObj = New-Object -ComObject ("WScript.Shell")
+
+$WScriptObj = New-Object -ComObject "WScript.Shell"
 $shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
 $shortcut.TargetPath = $SourceFilePath
 $shortcut.WorkingDirectory = Split-Path -Parent $SourceFilePath
