dc62ab224f35bb21975f128aa1bb8e377fa1afe9
Norman Dankert <norman.dankert@outlook.com>
2022-11-20 14:35:04 +0100

    add prompt and replacement for variables

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -1,5 +1,3 @@
-# ask for data for certain files
-# email for git, npm login?, project directory, editor for git
 [CmdletBinding(SupportsShouldProcess)]
 param ()
 
@@ -7,8 +5,17 @@ $yesToAll = $false
 $noToAll = $false
 
 $dotfiles = "dotfiles"
+$configPath = "$HOME/.config/dotfiles.json"
+$pattern = [Regex]"<<(?<variable>\w+)>>"
 $basePath = [System.IO.Path]::Combine($PSScriptRoot, $dotfiles)
 $files = Get-ChildItem -Recurse -File $dotfiles
+
+# Make sure the config file already exists
+if (-not (Test-Path $configPath)) {
+	New-Item $configPath -ItemType File -Value "{}" -Force
+}
+
+$config = Get-Content $configPath | ConvertFrom-Json
 
 foreach ($file in $files) {
 	$relativePath = [System.IO.Path]::GetRelativePath($basePath, $file.FullName)
@@ -18,6 +25,26 @@ foreach ($file in $files) {
 		continue
 	}
 
-	Write-Output "Copying $relativePath to $targetPath"
-	Copy-Item $file.FullName $targetPath
+	$content = Get-Content $file.FullName -Raw
+	$allMatches = $pattern.Matches($content)
+
+	if ($null -ne $allMatches) {
+		foreach ($match in $allMatches) {
+			$varName = $match.Groups["variable"].Value
+			$varValue = $config.PSObject.Properties[$varName]?.Value
+
+			if ($null -eq $varValue) {
+				$varValue = Read-Host "Please enter value for '$varName'"
+				$config | Add-Member -MemberType NoteProperty -Name $varName -Value $varValue
+			}
+
+			Write-Output "Replacing '$varName' with '$varValue"
+			$content = ([Regex]"<<$varName>>").Replace($content, $varValue, 1)
+		}
+	}
+
+	Write-Output "Saving $relativePath to $targetPath"
+	New-Item $targetPath -ItemType File -Value $content -Force | Out-Null
 }
+
+Set-Content $configPath ($config | ConvertTo-Json)
