1208fef1ca69409852401fc91c84ec28244be68d
Norman Dankert <norman.dankert@outlook.com>
2023-10-20 19:07:36 +0200

    use tar'd Nerd Fonts

--- dotfiles/Documents/PowerShell/Scripts/Install-NerdFont.ps1
+++ dotfiles/Documents/PowerShell/Scripts/Install-NerdFont.ps1
@@ -5,21 +5,23 @@ param (
 	[string]$DownloadUrl = "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
 )
 
-$TempFile = [System.IO.Path]::GetTempFileName()
-$TempDirectory = Join-Path $Env:Temp $(New-Guid)
+$temp = [System.IO.Path]::GetTempFileName()
+$tempDirectory = Join-Path $Env:Temp $(New-Guid)
+New-Item -ItemType Directory -Path $tempDirectory | Out-Null
 
 try {
-	Invoke-WebRequest -Uri "$DownloadUrl$FontName.zip" -OutFile $TempFile
-	Expand-Archive -LiteralPath $TempFile -DestinationPath $TempDirectory
+	Invoke-WebRequest -Uri "$DownloadUrl$FontName.tar.xz" -OutFile $temp
+	tar xzf $temp -C $tempDirectory
 
-	$fonts = (New-Object -ComObject shell.application).NameSpace(0x14)
-	foreach ($fontFile in (Get-ChildItem $TempDirectory -Recurse -Include "*.ttf", "*.otf")) {
+	$fonts = (New-Object -ComObject Shell.Application).NameSpace(0x14)
+	foreach ($fontFile in (Get-ChildItem $tempDirectory -Recurse -Include "*.ttf", "*.otf")) {
 		if ($PSCmdlet.ShouldProcess($fontFile.Name, "Install font")) {
-			$fonts.CopyHere($fontFile.FullName)
+			Write-Output "Installing $($fontFile.Name)"
+			$fonts.CopyHere($fontFile.FullName, 0x14)
 		}
 	}
 }
 finally {
-	Remove-Item $TempFile
-	Remove-Item $TempDirectory -Recurse
+	Remove-Item $temp
+	Remove-Item $tempDirectory -Recurse
 }
