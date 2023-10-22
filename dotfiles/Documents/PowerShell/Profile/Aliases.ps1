d3d7d17003c08565c9f4bae0bcdab7b9ee84582a
Norman Dankert <norman.dankert@outlook.com>
2023-10-22 05:06:22 +0200

    add documentation for the open alias

--- dotfiles/Documents/PowerShell/Profile/Aliases.ps1
+++ dotfiles/Documents/PowerShell/Profile/Aliases.ps1
@@ -1,4 +1,23 @@
-# Open anything from the command line
+<#
+.SYNOPSIS
+	Opens a file or folder from the command line.
+
+.DESCRIPTION
+	The 'open' function allows you to quickly open a file or folder from the command line by passing in the path as an argument.
+
+	Defaults to the current directory if no path is provided.
+
+.PARAMETER Path
+	The path to the file or folder that you want to open.
+
+.EXAMPLE
+	open C:\Users\Username\Documents\example.txt
+	This command will open the 'example.txt' file in Notepad.
+
+.EXAMPLE
+	open C:\Users\Username\Documents
+	This command will open the 'Documents' folder in File Explorer.
+#>
 Function open {
 	param (
 		[string]$Path = "."
