3ebfff72b79478d9efefbc9b6546d57fc1ccc616
Norman Dankert <norman.dankert@outlook.com>
2024-03-22 22:08:35 +0100

    Add documentation to sort-gitconfig script

--- scripts/Sort-GitConfig.ps1
+++ scripts/Sort-GitConfig.ps1
@@ -1,13 +1,28 @@
+<#
+.SYNOPSIS
+    Sorts the sections in a gitconfig file.
+
+.DESCRIPTION
+    This script reads a gitconfig file, sorts the sections, and writes the sorted sections back to the file.
+
+.PARAMETER Path
+    The path to the gitconfig file to sort.
+
+.EXAMPLE
+    Sort-GitConfig.ps1 -Path "~/.gitconfig"
+    This command sorts the sections in the gitconfig file at "~/.gitconfig".
+#>
+
 [CmdletBinding()]
 param (
 	[Parameter(Mandatory)]
-	[string]$GitConfigPath
+	[string]$Path
 )
 
-if (-not (Test-Path $GitConfigPath -PathType Leaf)) {
+if (-not (Test-Path $Path -PathType Leaf)) {
 	Write-Error "Invalid gitconfig path"
 	exit
 }
 
-$gitConfig = [System.Collections.SortedList](Get-IniContent $GitConfigPath)
-Out-IniFile -InputObject $gitConfig -FilePath $GitConfigPath -Pretty -Loose -Force
+$gitConfig = [System.Collections.SortedList](Get-IniContent $Path)
+Out-IniFile -InputObject $gitConfig -FilePath $Path -Pretty -Loose -Force
