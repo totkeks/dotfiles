aeb77a0fd83f56ce9f50d661b0e7811ffb41c2bb
Norman Dankert <norman.dankert@outlook.com>
2024-03-22 22:05:32 +0100

    Refactor Write-Greeting script to improve readability and modularity

--- dotfiles/Documents/PowerShell/Scripts/Write-Greeting.ps1
+++ dotfiles/Documents/PowerShell/Scripts/Write-Greeting.ps1
@@ -1,11 +1,25 @@
-$username = $IsWindows ? (Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\SessionData\" |
+function Get-LoggedOnDisplayName {
+	Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\SessionData\" |
 		Get-ItemProperty |
 		Where-Object { $_.psobject.Properties["LoggedOnUser"] -Like "*$($env:USERNAME)" } |
-		Select-Object -First 1 -ExpandProperty LoggedOnDisplayName) : $env:USERNAME
+		Select-Object -First 1 -ExpandProperty LoggedOnDisplayName
+}
 
+function Write-Greeting {
+	param (
+		[string]$username,
+		[string]$computername,
+		[string]$os,
+		[string]$psversion
+	)
+
+	Write-Output "Hello $($PSStyle.Foreground.Blue)$username$($PSStyle.Reset), welcome to $($PSStyle.Foreground.Blue)$computername$($PSStyle.Reset)!"
+	Write-Output "You are running $($PSStyle.Foreground.Yellow)PowerShell $psversion$($PSStyle.Reset) on $($PSStyle.Foreground.Yellow)$os$($PSStyle.Reset).`n"
+}
+
+$username = $IsWindows ? (Get-LoggedOnDisplayName) : $env:USERNAME
 $computername = $env:COMPUTERNAME
 $os = $PSVersionTable.OS
 $psversion = $PSVersionTable.PSVersion
 
-Write-Output "Hello $($PSStyle.Foreground.Blue)$username$($PSStyle.Reset), welcome to $($PSStyle.Foreground.Blue)$computername$($PSStyle.Reset)!"
-Write-Output "You are running $($PSStyle.Foreground.Yellow)PowerShell $psversion$($PSStyle.Reset) on $($PSStyle.Foreground.Yellow)$os$($PSStyle.Reset).`n"
+Write-Greeting -username $username -computername $computername -os $os -psversion $psversion
