b26fd45a689944745989566bc9206941d54fd4ab
Norman Dankert <norman.dankert@outlook.com>
2024-04-13 01:01:58 +0200

    Lazy-load completions that use external tools

--- dotfiles/Documents/PowerShell/Profile/TabCompletions.ps1
+++ dotfiles/Documents/PowerShell/Profile/TabCompletions.ps1
@@ -8,8 +8,6 @@ Import-Module CompletionPredictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-Import-Module posh-git
-
 # .NET CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 	param($wordToComplete, $commandAst,	$cursorPosition)
@@ -28,14 +26,23 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
 	}
 }
 
-# Oh My Posh
-oh-my-posh completion powershell | Out-String | Invoke-Expression
+# Lazy-load additional completions that require external tools
+Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -Action {
+	# Git (TODO reduce to just completions, without the prompt features)
+	Import-Module posh-git
 
-# GitHub CLI
-gh completion -s powershell | Out-String | Invoke-Expression
+	# Oh My Posh
+	oh-my-posh completion powershell | Out-String | Invoke-Expression
 
-# Podman, with workaround for https://github.com/containers/podman/issues/15527
-(podman completion powershell | Out-String) -replace "podman.exe", "podman" | Invoke-Expression
+	# GitHub CLI
+	gh completion -s powershell | Out-String | Invoke-Expression
 
-# Volta
-volta completions powershell | Out-String | Invoke-Expression
+	# Podman, with workaround for https://github.com/containers/podman/issues/15527
+	(podman completion powershell | Out-String) -replace "podman.exe", "podman" | Invoke-Expression
+
+	# Volta
+	volta completions powershell | Out-String | Invoke-Expression
+
+	# Unregister the event after the completions have been loaded
+	Unregister-Event -SourceIdentifier PowerShell.OnIdle
+} | Out-Null
